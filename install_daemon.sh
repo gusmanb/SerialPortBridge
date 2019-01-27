6f256845451cbb4cbaa550b5ab5680d1ada316fe
Agustín Gimenez <geniwab@gmail.com>
2019-01-27 16:23:26 +0100

    Corrected install script eol

--- install_daemon.sh
+++ install_daemon.sh
@@ -3,4 +3,4 @@
 chmod 777 SerialLinuxHost
 echo -e "#!/bin/bash\n\ncd $PWD\nsudo ./SerialLinuxHost daemon &" > daemon
 chmod 777 daemon
-sed  "/exit 0/ { N; s/exit 0\n/$PWD/SerialLinuxHost\n&/ }" /etc/rc.local
+sed  -i "/^exit 0/ i $PWD\/daemon\n" /etc/rc.local 
